63f22fb22e1036d37646322694638b0df0ca8036
Indiel <kori.yanagi@gmail.com>
2020-05-05 10:46:40 +0300

    Updates

---
+++ precache-manifest.53f404fc0ac933f5a70da40516235b01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74e1d194585744d8b894dcb7200591cb",
+    "url": "/todo-react/index.html"
+  },
+  {
+    "revision": "6700aa6bb39873945da6",
+    "url": "/todo-react/static/css/main.653e485c.chunk.css"
+  },
+  {
+    "revision": "23e6e7645b196a6f3c6a",
+    "url": "/todo-react/static/js/2.9f835767.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/todo-react/static/js/2.9f835767.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6700aa6bb39873945da6",
+    "url": "/todo-react/static/js/main.db3753a2.chunk.js"
+  },
+  {
+    "revision": "8c8404ddc91811c2534b",
+    "url": "/todo-react/static/js/runtime-main.c528bf7d.js"
+  }
+]);
